65f85ba630733eb13a24dc4ce3b574022b3f36d6
astroLAC <astrolac@gmail.com>
2016-03-18 11:59:15 +0300

    Some changes

---
+++ videotest.php
@@ -0,0 +1,14 @@
+<html lang="ru">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="videotest.css" type="text/css">
+    <title>videoTEST</title>
+  </head>
+  <body bottommargin="0" leftmargin="0" rigthmargin="0" topmargin="0">
+    <div class="mainblock">
+      <?php
+        echo $_SERVER['HTTP_USER_AGENT'];
+      ?>
+    </div>
+  </body>
+</html>
