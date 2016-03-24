b5ec26b7c6445b8699df22bbd0606e4c6c525a01
astrolac <astrolac@gmail.com>
2016-03-24 10:41:09 +0300

    Some changes

--- videotest.php
+++ videotest.php
@@ -5,11 +5,22 @@
     <title>videoTEST</title>
   </head>
   <body bottommargin="0" leftmargin="0" rigthmargin="0" topmargin="0">
-    <div class="mainblock">
+    <div class="mainblock"> videoTEST <br />
       <?php
-        SQLiteDatabase sqlite_factory("db/videotest.db");
-        
+        $db = new SQLite3('db/videotest.db');
+        $results = $db->query('SELECT * FROM videos;');
+        while ($row = $results->fetchArray()) { ?>
+          <div class="mediablock">
+            <?php echo $row["id"]; ?>
+          <br />
+            <?php echo $row["title"]; ?>
+          <br />
+            <?php echo $row["path"].$row["filename"]; ?>
+          </div>
+      <?php
+        }
       ?>
+      </table>
     </div>
   </body>
 </html>
